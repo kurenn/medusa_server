4ba79d420de132e9ec64b1b44c522b62ee3c7e38
Abraham Kuri <kurenn@icalialabs.com>
2017-03-16 17:27:26 -0600

    Fixes a bug by stacking more than 2 images

--- lib/medusa/medusa.ex
+++ lib/medusa/medusa.ex
@@ -10,15 +10,23 @@ defmodule Medusa do
   end
 
   defp transform_to_base64(file_name) do
-    imagetobase64("/tmp/#{file_name}.jpg", "/tmp/#{file_name}")
+    imagetobase64("/tmp/#{file_name}.png", "/tmp/#{file_name}")
   end
 
   defp convert(image_paths, file_name) do
-    System.cmd "convert", Enum.concat(image_paths, conversion_arguments(file_name)), stderr_to_stdout: true
+    System.cmd "convert", Enum.concat(arguments(image_paths), conversion_arguments(file_name)), stderr_to_stdout: true
     transform_to_base64(file_name)
   end
 
+  defp arguments(image_paths) do
+    [head | tail] = image_paths
+
+    composited_urls = Enum.map(tail, fn(url) -> [url, "-composite"] end)
+
+    List.flatten [head | composited_urls]
+  end
+
   defp conversion_arguments(file_name) do
-    ["-gravity", "center", "-composite", "/tmp/#{file_name}.jpg"]
+    ["/tmp/#{file_name}.png"]
   end
 end
